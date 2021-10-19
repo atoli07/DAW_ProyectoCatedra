25050d7d7f1cc81b6cfd58afb6363e172e8cc6c4
Samuel-Pana <89950478+Samuel-Pana@users.noreply.github.com>
2021-10-18 21:11:13 -0600

    información en matrices
    Información en matrices

---
+++ js/Matrices.js
@@ -0,0 +1,110 @@
+window.onload=function()
+{
+    var elemento=document.getElementById("SLV1343");
+    elemento.onmouseover = function(e){
+
+        // El contenido de esta funcion se ejecutara cuanso el mouse
+        // pase por encima del elemento
+        alert(departamentos.Ahuchapan);
+
+    };
+    var elemento=document.getElementById("SLV1344");
+    elemento.onmouseover = function(e){
+
+
+        alert(departamentos.Cabañas);
+    };
+    var elemento=document.getElementById("SLV1345");
+    elemento.onmouseover = function(e){
+
+
+        alert(departamentos.Cuscatlán);
+    };
+    var elemento=document.getElementById("SLV1346");
+    elemento.onmouseover = function(e){
+
+        
+        alert(departamentos.LaLibertad);
+    };
+    var elemento=document.getElementById("SLV1347");
+    elemento.onmouseover = function(e){
+
+        
+        alert(departamentos.LaPaz);
+    };
+    var elemento=document.getElementById("SLV1348");
+    elemento.onmouseover = function(e){
+
+        
+        alert(departamentos.Sonsonate);
+    };
+    var elemento=document.getElementById("SLV1349");
+    elemento.onmouseover = function(e){
+
+
+        alert(departamentos.SanSalvador);
+    };
+    var elemento=document.getElementById("SLV1350");
+    elemento.onmouseover = function(e){
+
+
+        alert(departamentos.LaUnión);
+    };
+
+    var elemento=document.getElementById("SLV1351");
+    elemento.onmouseover = function(e){
+
+
+        alert(departamentos.Morazán);
+    };
+    var elemento=document.getElementById("SLV1352");
+    elemento.onmouseover = function(e){
+
+        alert(departamentos.SanMiguel);
+    };
+    var elemento=document.getElementById("SLV1353");
+    elemento.onmouseover = function(e){
+
+        alert(departamentos.SanVicente);
+    };
+    var elemento=document.getElementById("SLV1354");
+    elemento.onmouseover = function(e){
+
+        alert(departamentos.Usulután);
+    };
+    var elemento=document.getElementById("SLV1357");
+    elemento.onmouseover = function(e){
+
+        alert(departamentos.Chalatenango);
+    };
+    var elemento=document.getElementById("SLV1358");
+    elemento.onmouseover = function(e){
+
+        alert(departamentos.SantaAna);
+    };
+
+
+
+
+
+
+
+    var  departamentos = new Array(14);
+    departamentos["Ahuchapan"] = ["Cabezara departamental: Ahuachapán", "Extensión territorial: 756.19 km cuadrados.", "Fiestas: del 5 al 14 de febrero", "Habitantes: para el año 2017 eran 129,750"];
+    departamentos["Cabañas"] = ["Cabezera departamental: Sensuntepeque", "Extensión territorial: 1,103.51 km cuadrados", "Fiestas: En honor a San Miguel Arcángel (20-29 de septiembre)", "Número de habitantes: 167,761 habitantes"];
+    departamentos["Cuscatlán"] = ["Cabezara departamental: Cojutepeque", "Extensión territorial: 756.19 km cuadrados", "Fiestas: oficialmente los días 26, 27 y 28 de diciembre", "Habitantes: para el año 2018 eran 232,238"];
+    departamentos["LaLibertad"] = ["Cabezara departamental: Santa Tecla.", "Extensión territorial: 1,652.88 km cuadrados", "Fiestas: del 1 al 8 de diciembre", "Habitantes: para el año 2018 eran 843 500"];
+    departamentos["LaPaz"] = ["Cabezera departamental: Zacatecoluca", "Extensión territorial: 1,223.61 km cuadrados", "Fiestas: del 16 al 26 de febrero.", "Número de habitantes: 362,649 habitantes"];
+    departamentos["Sonsonate"] = ["Cabezara departamental: Sonsonate", "Extensión territorial: 1,225.77 km cuadrados", "Fiestas: del 25 de enero al 2 de febrero", "Habitantes: para el año 2013 eran 438 960."];
+    departamentos["SanSalvador"] = ["Cabezara departamental: San Salvador", "Extensión territorial: 886.15 km cuadrados", "Fiestas: del 1 al 6 de agosto", "Habitantes: para el año 2019 eran 2,750,600"];
+    departamentos["LaUnión"] = ["Cabezera departamental: ciudad de La Unión", "Extensión territorial: 2,074 km cuadrados", "Fiestas: del 1 al 15 de octubre.", "Número de habitantes: 268,557 habitantes"];
+    departamentos["Morazán"] = ["Cabezera departamental: San Francisco Gotera", "Extensión territorial: 1,447.43 km cuadrados", "Fiestas: del 1 al 4 de octubre", "Número de habitantes: 203,677"];
+    departamentos["SanMiguel"] = ["Cabezera departamental: ciudad de San Miguel", "Extensión territorial: 2,077.10 km cuadrados", "Fiestas: Último fin de semana de noviembre", "Número de habitantes: 500,062 habitantes"];
+    departamentos["SanVicente"] = ["Cabezera departamental: ciudad de San Vicente", "Extensión territorial: 1,184.02 km cuadrados", "Fiestas: del 10 al 31 de diciembre", "Número de habitantes: 184,093 habitantes"];
+    departamentos["Usulután"] = ["Cabezera departamental: ciudad de Usulután", "Extensión territorial: 2,130.44 km cuadrados", "Fiestas: del 16 al 29 de noviembre", "Número de habitantes: 374,629 habitantes"];
+    departamentos["Chalatenango"] = ["Cabezara departamental: Chalatenango", "Extensión territorial: 2,016.58 km cuadrados", "Fiestas: del 15 al 24 de junio.", "Habitantes: para el año 2017 eran 275.000"];
+    departamentos["SantaAna"] = ["Cabezara departamental: Santa Ana", "Extensión territorial: 2,023.17 km cuadrados", "Fiestas: del 17 al 26 de julio", "Habitantes: para el año 2013 eran 264 091."];
+
+
+
+}
